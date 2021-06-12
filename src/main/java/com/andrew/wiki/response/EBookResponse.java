fffc2fd159dce0a4c175cfeb2b4353cda889ced1
fengliu1227 <fengliu1227@outlook.com>
2021-06-12 00:29:09 -0400

    ebook response class

---
+++ src/main/java/com/andrew/wiki/response/EBookResponse.java
@@ -0,0 +1,28 @@
+package com.andrew.wiki.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EBookResponse {
+
+    private String name;
+
+    private Long category1Id;
+
+    private Long category2Id;
+
+    private String description;
+
+    private String cover;
+
+    private Integer docCount;
+
+    private Integer viewCount;
+
+    private Integer voteCount;
+
+}
--- src/main/java/com/andrew/wiki/service/EBookService.java
+++ src/main/java/com/andrew/wiki/service/EBookService.java
@@ -3,9 +3,12 @@ package com.andrew.wiki.service;
 import com.andrew.wiki.domain.EBook;
 import com.andrew.wiki.domain.EBookExample;
 import com.andrew.wiki.mapper.EBookMapper;
+import com.andrew.wiki.response.EBookResponse;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,5 +19,19 @@ public class EBookService {
 
     public List<EBook> getAll(){
         return eBookMapper.selectByExample(new EBookExample());
+    }
+
+    public List<EBookResponse> searchByKeyword(String keyword){
+        EBookExample eBookExample = new EBookExample();
+        EBookExample.Criteria criteria = eBookExample.createCriteria();
+        criteria.andNameLike("%" + keyword + "%");
+        List<EBook> list = eBookMapper.selectByExample(eBookExample);
+        List<EBookResponse> resList = new ArrayList<>();
+        for(EBook ebook: list){
+            EBookResponse eBookResponse = new EBookResponse();
+            BeanUtils.copyProperties(ebook,eBookResponse);
+            resList.add(eBookResponse);
+        }
+        return resList;
     }
 }
