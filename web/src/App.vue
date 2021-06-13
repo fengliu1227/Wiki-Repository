d6fa9dee3d2edb087932c94b59ede064f30e6806
fengliu1227 <fengliu1227@outlook.com>
2021-06-12 21:40:58 -0400

    the-footer component

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,20 +2,20 @@
   <a-layout id="components-layout-demo-top-side-2">
     <the-header/>
     <router-view/>
-    <a-layout-footer style="text-align: center">
-      Andrew's wiki repository
-    </a-layout-footer>
+    <the-footer/>
   </a-layout>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue';
   import TheHeader from '@/components/the-header.vue';
+  import TheFooter from '@/components/the-footer.vue';
 
   export default defineComponent({
     name: 'app',
     components:{
       TheHeader,
+      TheFooter,
     }
   });
 </script>
