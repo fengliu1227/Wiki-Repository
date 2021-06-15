97b4748a4140af6d8de2f515252bc0599eda525b
fengliu1227 <fengliu1227@outlook.com>
2021-06-15 01:31:21 -0400

    add add doc function

--- web/src/views/admin/Admin-Doc.vue
+++ web/src/views/admin/Admin-Doc.vue
@@ -60,7 +60,8 @@
       <a-input v-model:value="doc.name" />
     </a-form-item>
 
-    <a-form-item label="名称">
+
+    <a-form-item label="Parent Node">
 
       <a-tree-select
 
@@ -101,11 +102,20 @@
   import axios from 'axios';
   import { message } from 'ant-design-vue';
   import { Tool } from '@/util/tool';
+  import {useRoute} from "vue-router";
 
 
   export default defineComponent({
     name: 'AdminDoc',
     setup() {
+      const route = useRoute();
+      // console.log("route：", route);
+      // console.log("route.path：", route.path);
+      // console.log("route.query：", route.query);
+      // console.log("route.param：", route.params);
+      // console.log("route.fullPath：", route.fullPath);
+      // console.log("route.name：", route.name);
+      // console.log("route.meta：", route.meta);
       const param = ref();
       param.value={};
       const docs = ref();
@@ -248,7 +258,9 @@
        */
       const add = () => {
         modalVisible.value = true;
-        doc.value = {};
+        doc.value = {
+          ebookId : route.query.ebookId
+        };
 
         treeSelectData.value = Tool.copy(level1.value);
 
